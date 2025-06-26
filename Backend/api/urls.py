ed0fe7b9049754c6677693ce1bd8ea77f8ff930a
AKIL A <akilaskarali@gmail.com>
2025-06-26 14:15:04 +0530

    Feat: Web scrap using Duckduckgo

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('smart-planner/', smart_planner, name='ai_travel_suggestions'),   
     path('get-all-festivals/', get_all_festivals, name='get_all_festivals'), 
     path("fetch-reddit-reviews/", fetch_reddit_reviews_by_id, name="fetch_reddit_reviews"),
+    path("enhance-festival-ai/", enhance_festival_ai, name="enhance_festival_ai"),
     path("generate-voice-briefing/", generate_voice_briefing, name="generate_voice_briefing"),
     path("generate-ai-video/", generate_ai_video, name="generate_ai_video"),
 
